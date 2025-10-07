5a70c9b1e259d4255b3b17b69839f93463539550
rizk.gendi <rgendy@ejada.com>
2025-10-07 16:37:28 +0300

    add accessablity attributes

--- src/components/AssistiveTechInfo.jsx
+++ src/components/AssistiveTechInfo.jsx
@@ -1,7 +1,7 @@
 
 export default function AssistiveTechInfo({ emojiData, matchingCards }) {
     return (
-        <section className="sr-only">
+        <section className="sr-only" aria-live="polite" aria-atomic={true}>
             <h1>Game Status</h1>
             <p>{`Number of matched pairs: ${(matchingCards.length) / 2}.`}</p>
             <p>{`Number of cards left to match: ${emojiData.length - matchingCards.length}.`}</p>
