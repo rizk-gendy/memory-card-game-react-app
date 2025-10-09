bd31483f0c1fa05f434930eb027506878cdc89a6
rizk.gendi <rgendy@ejada.com>
2025-10-09 17:15:30 +0300

    update the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,34 @@
 import RegularButton from "./RegularButton";
 
-export default function Form({ handleSubmit }) {
+export default function Form({ handleSubmit, handleChange }) {
 
     return (
-        <form className="wrapper">
-            <RegularButton handleClick={handleSubmit}>
-                Start Game
-            </RegularButton>
-        </form>
+        <div className="form-container" >
+            <form className="wrapper">
+            <div className="form__inner-wrapper">
+                    <label htmlFor="category">Select Emoji Category</label>
+                    <select name="category" id="category" onChange={handleChange}>
+                        <option value="animals-and-nature">animals-and-nature</option>
+                        <option value="food-and-drink">food-and-drink</option>
+                        <option value="travel-and-places">travel-and-places</option>
+                        <option value="objects">objects</option>
+                        <option value="symbols">symbols</option>
+                    </select>
+                </div>
+                <div className="form__inner-wrapper">
+                    <label htmlFor="number">Select the number of  memory Card</label>
+                    <select name="number" id="number" onChange={handleChange}>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="30">30</option>
+                        <option value="40">40</option>
+                        <option value="50">50</option>
+                    </select>
+                </div>
+                <RegularButton handleClick={handleSubmit}>
+                    Start Game
+                </RegularButton>
+            </form>
+        </div>
     )
 }
