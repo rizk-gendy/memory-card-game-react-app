49dfd830b1281a806f22c62a100175ebbe168b5f
rizk.gendi <rgendy@ejada.com>
2025-10-09 14:53:29 +0300

    make the game over comp focusable to support assistibe technology

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,15 @@
+import { useEffect, useRef } from "react";
 import RegularButton from "./RegularButton";
 
 export default function GameOver({ handleClick }) {
 
+    const gameOverRef = useRef(null)
+    useEffect(() => {
+        gameOverRef.current.focus()
+
+    }, [])
     return (
-        <div className="wrapper wrapper--accent">
+        <div className="wrapper wrapper--accent" ref={gameOverRef} tabIndex={-1}>
             <p className="p--large">
                 You've matched all the memory cards!
             </p>
