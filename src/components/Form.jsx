023e47b697b96894ed95d8b9b23224370c0f5c04
rizk.gendi <rgendy@ejada.com>
2025-10-14 14:56:16 +0300

    add select compontent and refactor Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,22 @@
+import { data } from "../data/data";
 import RegularButton from "./RegularButton";
+import Select from "./Select";
 
 export default function Form({ handleSubmit, handleChange }) {
 
     return (
         <div className="form-container" >
             <form className="wrapper">
-            <div className="form__inner-wrapper">
-                    <label htmlFor="category">Select Emoji Category</label>
-                    <select name="category" id="category" onChange={handleChange}>
-                        <option value="animals-and-nature">animals-and-nature</option>
-                        <option value="food-and-drink">food-and-drink</option>
-                        <option value="travel-and-places">travel-and-places</option>
-                        <option value="objects">objects</option>
-                        <option value="symbols">symbols</option>
-                    </select>
-                </div>
-                <div className="form__inner-wrapper">
-                    <label htmlFor="number">Select the number of  memory Card</label>
-                    <select name="number" id="number" onChange={handleChange}>
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="30">30</option>
-                        <option value="40">40</option>
-                        <option value="50">50</option>
-                    </select>
-                </div>
+                <Select
+                    label="Select Emoji Category"
+                    handleChange={handleChange}
+                    options={data.category}
+                />
+                <Select
+                    label="Select the number of  memory Card"
+                    handleChange={handleChange}
+                    options={data.number}
+                />
                 <RegularButton handleClick={handleSubmit}>
                     Start Game
                 </RegularButton>
